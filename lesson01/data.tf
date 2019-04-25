8a4b8864b6cb850585ef4bed64f38fcfc73246c3
Luis Carbajal <luis.carbajal@wizeline.com>
2019-04-25 15:06:09 -0500

    Add template_file data to render the user_data template

--- lesson01/data.tf
+++ lesson01/data.tf
@@ -19,3 +19,12 @@ data "aws_ami" "amazon_linux" {
 
   owners = ["amazon"]
 }
+
+data "template_file" "user_data" {
+  template = "${file("${path.module}/templates/user_data.sh.tpl")}"
+
+  vars = {
+    docker_image = "${var.app_name}"
+    docker_tag   = "${var.app_version}"
+  }
+}
